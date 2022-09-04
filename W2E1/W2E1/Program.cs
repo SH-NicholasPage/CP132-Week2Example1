b8ae79797b27285c4a63b7e778eb724b02260cef
SH-NicholasPage <npage@southhills.edu>
2022-09-03 22:52:24 -0400

    Initial commit

---
+++ W2E1/W2E1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+
+namespace W2E1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int myNumber;
+            int myAnswer;
+            Console.Write("Please enter a number: ");
+            myNumber = Convert.ToInt32(Console.ReadLine());
+            myAnswer = myNumber * 2;
+            Console.WriteLine(myNumber + " doubled is " + myAnswer);
+        }
+    }
+}
